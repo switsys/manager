8739acc7c88f033dcf6cb5fe4f71414864ca7c96
Marie JONES <marie.jones@corp.ovh.com>
2020-05-04 18:14:15 +0200

    feat(billing.history): revamp history display
    ref: MANAGER-4148
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/modules/models/index.js
+++ packages/manager/modules/models/index.js
@@ -1,9 +1,19 @@
+import Bill from './billing/Bill.class';
+import DebtAccount from './billing/DebtAccount.class';
 import BillingService from './BillingService/BillingService.class';
 import Certificate from './certificate/Certificate.class';
 import Price from './Price/Price.class';
 import Ticket from './support/ticket/ticket.class';
 import User from './user/User.class';
 
-export { BillingService, Certificate, Price, Ticket, User };
+export { Bill, BillingService, Certificate, DebtAccount, Price, Ticket, User };
 
-export default { BillingService, Certificate, Price, Ticket, User };
+export default {
+  Bill,
+  BillingService,
+  Certificate,
+  DebtAccount,
+  Price,
+  Ticket,
+  User,
+};
