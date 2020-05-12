1806e003b9453e90c1220fad46ef0dd5d86ce5c0
Frederic Espiau <frederic.espiau@corp.ovh.com>
2020-05-12 10:53:53 +0200

    feat(hosting.database.order.public): make it possible
    to carry out an order with Agora
    
    Signed-off-by: Frederic Espiau <frederic.espiau@corp.ovh.com>

--- packages/manager/apps/web/client/app/hosting/database/hosting-database.module.js
+++ packages/manager/apps/web/client/app/hosting/database/hosting-database.module.js
@@ -1,9 +1,15 @@
-import routing from './hosting-database.routing';
+import orderPrivate from './order/private';
+import orderPublic from './order/public';
 
-import orderPrivate from './order-private/hosting-database-order-private.module';
+import HostingDatabaseOrderPublic from './order/public/hosting-database-order-public.service';
+
+import routing from './hosting-database.routing';
 
 const moduleName = 'ovhManagerHostingDatabase';
 
-angular.module(moduleName, [orderPrivate]).config(routing);
+angular
+  .module(moduleName, [orderPrivate, orderPublic])
+  .service('HostingDatabaseOrderPublicService', HostingDatabaseOrderPublic)
+  .config(routing);
 
 export default moduleName;
